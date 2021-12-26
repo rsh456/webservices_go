aed7a6904da9a366a6f3f78c7faa21c335e463d3
rsh456 <rsh456@hotmail.com>
2021-12-25 19:43:47 -0500

    get products first version

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Product struct {
+	ProductID      int    `json:"productID"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
+}
+
+var productList []Product
+
+func init() {
+	productsJSON := `[
+		{
+			"productId":1,
+			"manufacturer": "Johns-Jenkins",
+    		"sku": "p5z343vdS",
+    		"upc": "939581000000",
+    		"pricePerUnit": "497.45",
+    		"quantityOnHand": 9703,
+    		"productName": "sticky note"
+		},
+		{
+			"productId": 2,
+			"manufacturer": "Hessel, Schimmel and Feeney",
+			"sku": "i7v300kmx",
+			"upc": "740979000000",
+			"pricePerUnit": "282.29",
+			"quantityOnHand": 9217,
+			"productName": "leg warmers"
+		}
+	]`
+	err := json.Unmarshal([]byte(productsJSON), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJson, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJson)
+	}
+}
+
+type fooHandler struct {
+	Message string
+}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(f.Message))
+
+}
+
+func main() {
+	http.HandleFunc("/products", productsHandler)
+	http.ListenAndServe(":5000", nil)
+}
